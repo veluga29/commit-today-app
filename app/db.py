37333962222bcf84de83cbb74cf458acef1603f4
lucian <sungyoon606@naver.com>
2023-10-12 21:51:21 +0900

    Rearrange line of async_session_factory

--- app/db.py
+++ app/db.py
@@ -8,14 +8,9 @@ from app import settings
 
 
 engine = create_async_engine(settings.POSTGRES_SETTINGS.get_dsn(), future=True)
+async_session_factory = async_sessionmaker(engine, expire_on_commit=False, autoflush=False, class_=AsyncSession)
 
 
 async def get_session():
-    async_session_factory = async_sessionmaker(
-        bind=engine,
-        expire_on_commit=False,
-        autoflush=False,
-        class_=AsyncSession,
-    )
     async with async_session_factory() as session:
         yield session
