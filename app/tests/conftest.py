d060275d66025ba9b4aae5385310a36aeffcd914
lucian <sungyoon606@naver.com>
2023-10-20 11:00:38 +0900

    Rearrange fixtures

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -3,7 +3,6 @@ from fastapi.testclient import TestClient
 from sqlalchemy import text
 from sqlalchemy.orm import sessionmaker, clear_mappers
 from sqlalchemy.ext.asyncio import (
-    AsyncEngine,
     AsyncSession,
     create_async_engine,
 )
@@ -16,7 +15,6 @@ from app.adapters.todo.persistent_orm import start_mappers as todo_start_mappers
 from app.adapters.user.persistent_orm import metadata as user_metadata
 from app.adapters.todo.persistent_orm import metadata as todo_metadata
 from app import settings
-from app.tests import exceptions
 
 
 @pytest_asyncio.fixture
@@ -41,39 +39,47 @@ async def create_test_db():
     async with async_engine.begin() as conn:
         await conn.execute(text(f"DROP DATABASE IF EXISTS {test_db}"))
         await conn.execute(text(f"CREATE DATABASE {test_db}"))
+
         yield
         await conn.execute(text(f"DROP DATABASE {test_db}"))
 
 
 @pytest_asyncio.fixture(scope="session")
-async def async_engine(create_test_db):
-    async_engine = create_async_engine(settings.POSTGRES_SETTINGS.get_test_dsn(), future=True)
-
-    yield async_engine
-
-    await async_engine.dispose()
-
-
-@pytest_asyncio.fixture(scope="function")
-async def async_session(async_engine):
+async def mappers(create_test_db):
     user_start_mappers()
     todo_start_mappers()
+    yield
+    clear_mappers()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_engine(mappers):
+    async_engine = create_async_engine(settings.POSTGRES_SETTINGS.get_test_dsn(), future=True)
 
     async with async_engine.begin() as conn:
         await conn.run_sync(user_metadata.create_all)
         await conn.run_sync(todo_metadata.create_all)
 
+        yield async_engine
+
+        await conn.run_sync(user_metadata.drop_all)
+        await conn.run_sync(todo_metadata.drop_all)
+
+    await async_engine.dispose()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_session_factory(async_engine):
     session_factory: sessionmaker = sessionmaker(
         async_engine,
         expire_on_commit=False,
         autoflush=False,
         class_=AsyncSession,
     )
-    async with session_factory() as session:
+    yield session_factory
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_session(async_session_factory):
+    async with async_session_factory() as session:
         yield session
-
-    async with async_engine.begin() as conn:
-        await conn.run_sync(user_metadata.drop_all)
-        await conn.run_sync(todo_metadata.drop_all)
-
-    clear_mappers()
