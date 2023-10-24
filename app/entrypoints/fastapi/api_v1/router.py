8e8bc605c53bbe43b1ddaca53ded7648525c3ca7
lucian <sungyoon606@naver.com>
2023-10-24 18:54:55 +0900

    Add basic API routers

---
+++ app/entrypoints/fastapi/api_v1/router.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+from app.entrypoints.fastapi.api_v1.todo.todo import router as todo_router
+from app.entrypoints.fastapi.api_v1.user.user import router as user_router
+from app import settings
+
+api_router = APIRouter(prefix=settings.API_V1_STR)
+external_router = APIRouter()
+internal_router = APIRouter()
+
+external_router.include_router(todo_router, prefix="/todo", tags=["todo"])
+external_router.include_router(user_router, prefix="/user", tags=["user"])
+
+api_router.include_router(external_router, prefix="/external")
+api_router.include_router(internal_router, prefix="/internal")
--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
 from app import settings
+from app.entrypoints.fastapi.api_v1.router import api_router as api_v1_router
 
 app = FastAPI(
     title="Commit Today: TODO List with Progress Visualization",
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
+
+app.include_router(api_v1_router)
 
 
 @app.get("/")
