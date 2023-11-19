604dacc7d1f2a784ebf3054027dc93369c8eee5b
lucian <sungyoon606@naver.com>
2023-11-19 12:41:00 +0900

    Change import path from  user to auth

--- app/db.py
+++ app/db.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import (
 )
 
 from app import settings
-from app.adapters.user.persistent_orm import start_mappers as user_start_mappers
+from app.adapters.auth.persistent_orm import start_mappers as user_start_mappers
 from app.adapters.todo.persistent_orm import start_mappers as todo_start_mappers
 
 
