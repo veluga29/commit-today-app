cec3405678c66e64495d2d78c7f452acbd3fbe47
lucian <sungyoon606@naver.com>
2023-12-04 23:43:18 +0900

    Add global exception handlers of fastapi

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError, HTTPException
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from app import settings
 from app.entrypoints.fastapi.api_v1.router import api_router as api_v1_router
+from app.entrypoints.fastapi.api_v1 import schemas
 
 app = FastAPI(
     title="Commit Today: TODO List with Progress Visualization",
@@ -9,6 +13,22 @@ app = FastAPI(
 )
 
 app.include_router(api_v1_router)
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=schemas.Response(ok=False, message=str(exc.detail), data=None).model_dump(),
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=schemas.Response(ok=False, message=str(exc), data=exc.errors()).model_dump(),
+    )
 
 
 @app.get("/")
