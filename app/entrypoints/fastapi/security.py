49cf3edd1e13a258126068eb177d9affc74a60a6
lucian <sungyoon606@naver.com>
2023-12-10 18:01:33 +0900

    Add authorization parser that read access token from cookie

--- app/entrypoints/fastapi/security.py
+++ app/entrypoints/fastapi/security.py
@@ -1,17 +1,49 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import Depends, HTTPException, status, Request
+from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
 from typing import Any
 
 from app import settings
 
 
+class JWTCookieAuth(OAuth2PasswordBearer):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: str | None = None,
+        scopes: dict[str, str] | None = None,
+        description: str | None = None,
+        auto_error: bool = True,
+    ):
+        super().__init__(
+            tokenUrl=tokenUrl,
+            scheme_name=scheme_name,
+            scopes=scopes,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> str | None:
+        if (access_token := request.cookies.get("access_token")) is None:
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+
+        return access_token
+
+
 class JWTAuthorizer:
     SECRET_KEY = settings.AUTH_SETTINGS.JWT_SECRET_KEY
     ALGORITHM = settings.AUTH_SETTINGS.JWT_ALGORITHM
     EXPIRES_DELTA = settings.AUTH_SETTINGS.JWT_EXPIRES_DELTA
+    JWT_COOKIE_AUTH = JWTCookieAuth(tokenUrl="api/v1/external/auth/login", auto_error=False)
 
     class CredentialsException(Exception):
         ...
@@ -39,12 +71,11 @@ class JWTAuthorizer:
         return jwt.decode(access_token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
 
     @classmethod
-    def get_user_info(cls, auth_header: HTTPAuthorizationCredentials | None = Depends(HTTPBearer(auto_error=False))):
+    async def get_user_info(cls, access_token: str | None = Depends(JWT_COOKIE_AUTH)):
         try:
-            if auth_header is None:
+            if access_token is None:
                 raise cls.CredentialsException
-            token = auth_header.credentials
-            payload = jwt.decode(token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
+            payload = jwt.decode(access_token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
             email, username, last_name, first_name = (
                 payload.get("sub"),
                 payload.get("username"),
@@ -58,7 +89,6 @@ class JWTAuthorizer:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Could not validate credentials",
-                headers={"WWW-Authenticate": "Bearer"},
             )
 
         return user_info
