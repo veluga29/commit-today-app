e33b9cfd213ffdb8683d5cd0a63f6eb0a1e36a85
lucian <sungyoon606@naver.com>
2023-12-18 00:01:45 +0900

    Add deprecated comment to custom event loop

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -17,6 +17,7 @@ from app import settings
 from app.db import get_session
 
 
+# TODO: deprecated for pytest-asyncio 0.23
 @pytest.fixture(scope="session")
 def event_loop():
     policy = asyncio.get_event_loop_policy()
@@ -24,6 +25,14 @@ def event_loop():
 
     yield loop
     loop.close()
+
+
+# TODO: upgrade pytest-asyncio 0.23
+# def pytest_collection_modifyitems(items):
+#     pytest_asyncio_tests = (item for item in items if pytest_asyncio.is_async_test(item))
+#     session_scope_marker = pytest.mark.asyncio(scope="session")
+#     for async_test in pytest_asyncio_tests:
+#         async_test.add_marker(session_scope_marker)
 
 
 @pytest_asyncio.fixture(scope="session")
